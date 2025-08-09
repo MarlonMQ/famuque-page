e755715cc853182a306ca2d61dd7d916fe1855e2
MarlonMQ <105015034+MarlonMQ@users.noreply.github.com>
2025-08-08 18:11:56 -0600

    feat(HomePage): update background images to WEBP format and enhance product section with new content

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,13 @@
-import FamuqueBackground from "@/assets/background/famuque-background--1.jpg"
+import FamuqueBackground from "@/assets/background/famuque-background--1.webp"
+import FamuqueBackground2 from "@/assets/background/famuque-background--2.webp"
 import {FamuqueNavBar} from "@/components/FamuqueNavBar"
 import { DefaultLayout } from "@/components/Layout/DefaultLayout"
 import { useNavigate } from "react-router-dom"
 import { FamuqueFooter } from "@/components/FamuqueFooter"
 import { FamuqueButton } from "@/components/FamuqueButton"
 import { FamuqueMetadata } from "@/components/FamuqueMetaData"
+import { FamuqueMarquee } from "@/components/FamuqueMarquee"
+import { brandsData } from "@/data/brandsData"
 
 function HomePage() {
   const navigate = useNavigate()
@@ -31,16 +34,48 @@ function HomePage() {
           </div>
         </section>
         <section className="bg-white flex flex-col relative w-full items-center justify-center py-comp-2">
-          <label className="text-gh-3 font-avenir-heavy">Nuestros Productos</label>
-          <div className="grid grid-cols-1 mobile:grid-cols-2 tablet:grid-cols-3 laptop:grid-cols-4 gap-std-3 p-comp-1 tablet:p-comp-1-desktop w-full max-w-screen-desktop">
+          <div className="flex flex-col items-center justify-center mx-3 max-w-screen-desktop">
+            <label className="text-th-3 medium:text-dh-1 text-center font-avenir-heavy text-gray">
+              Más de 20 años dedicados a proveer soluciones en sistemas de riego de alta calidad.
+            </label>
+            <br />
+            <label className="text-th-4 medium:text-dh-3 text-center font-avenir-light">
+              Distribuimos insumos certificados de marcas líderes, respaldando proyectos agrícolas e industriales en todo el país.
+            </label>
+            <FamuqueButton 
+              className="mt-comp-1"
+              variant="tertiary"
+              onClick={() => navigate('/catalog')}
+            >
+              Mira nuestros productos
+            </FamuqueButton>
           </div>
-          <FamuqueButton 
-            onClick={() => navigate('/catalog')}
-            variant="tertiary"
-          >
-            Ver más
-          </FamuqueButton>
+        </section>  
+        <section className="relative bg-white flex flex-row w-full h-minimal-2 medium:h-short items-center justify-center py-comp-2">
+          {/* contacto*/}
+          <img src={FamuqueBackground2} alt="Famuque" className="absolute w-full h-full object-cover brightness-50" />
+          <div className="w-full flex flex-col max-w-screen-desktop gap-comp-1 z-10 mx-std-2 bg-famuque-light items-center py-std-3 medium:py-comp-2  shadow-2xl">
+            <label className="text-th-2 medium:text-dh-1 text-center font-avenir-heavy text-famuque">
+              ¡Ponte en contacto con nosotros!
+            </label>
+            <label className="text-th-4 medium:text-dh-3 font-avenir-light text-center">
+              Para obtener más información sobre nuestros precios o productos
+            </label>
+            <FamuqueButton 
+              className="mt-comp-1 max-w-fit"
+              onClick={() => navigate('/contact')}
+              >
+              Contáctanos
+            </FamuqueButton>
+          </div>
+
         </section>
+        <FamuqueMarquee 
+          className="py-comp-1 bg-famuque" 
+          itemClassName="max-w-strap-mobile mx-std-3 medium:max-w-minimal-1 medium:mx-comp-1"
+          imageClassName="opacity-50 grayscale"
+          images={brandsData}
+        />
         <FamuqueFooter/>
       </DefaultLayout>
     </>
