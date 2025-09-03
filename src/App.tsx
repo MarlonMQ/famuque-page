1ab97bb4f4a4b98a2b9e221340ec329c7ac20c14
MarlonMQ <105015034+MarlonMQ@users.noreply.github.com>
2025-09-03 12:40:13 -0600

    fix(App): add console logs for environment mode and context

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import AppRouter from "@/router/Router";
 import { FamuqueToast } from "@/components/FamuqueToast";
 
 export default function App() {
+  console.log("MODE:", import.meta.env.MODE)       // "development" en branch deploy dev
+  console.log("CONTEXT:", process.env.CONTEXT)    // "branch-deploy" en Netlify
   return (
     <>
       <AppRouter />
       <FamuqueToast />
     </>
   );
+  
 }
